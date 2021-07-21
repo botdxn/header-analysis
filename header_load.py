7ff3e6709261d97d3dec79edf3a26bc8ed209af1
Marcin Kucharski <dixone23@gmail.com>
2021-07-21 21:23:36 +0200

    rozwijanie find_headers

--- header_load.py
+++ header_load.py
@@ -1,22 +1,26 @@
-#załadowanie pliku z parametru path i zwrócenie returnem do dalszego wykorzystania
+#załadowanie pliku z podanego parametru path i zwrócenie returnem do dalszego wykorzystania
 def load_file(path):
-    with open(path, 'r', encoding='utf-8') as file:
-        return file.readlines()
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            return file.readlines()
+    except Exception as e:
+        print(e)
+
 
 #wycięcie fragmentu z nagłówkami i zwrócenie części linijek aż do poczatku body wiadomości
-def find_headers(message):
-    
+def find_headers(message):    
     #usunięcie symboli newline
     message = [x.replace('\n','') for x in message]
     
-    #przeszukanie każdego wiersza w poszukiwaniu zahardcodowanej linijki
-    wiersz = 0
+    #przeszukanie każdego current_rowa w poszukiwaniu zahardcodowanej linijki
+    current_row = 0
     for row in message:
         if 'Content-Type: text/plain;' in str(row):
-            return message[0:wiersz-1] 
+            return message[0:current_row-1] 
         else:
-            wiersz += 1
+            current_row += 1
+
 
 #test
-plik = load_file('pliki_naglowkow//test.txt')
-print(find_headers(plik))
+test_file = load_file('pliki_naglowkow//test.txt')
+print(find_headers(test_file))
