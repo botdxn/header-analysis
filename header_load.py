1b46ab47949568f1d49ccbaa492820edd1e51b8d
Marcin Kucharski <dixone23@gmail.com>
2021-07-21 21:35:00 +0200

    Przeksztalcenie na klase zamiast uzywanie osobnych funkcji

--- header_load.py
+++ header_load.py
@@ -1,26 +1,32 @@
-#załadowanie pliku z podanego parametru path i zwrócenie returnem do dalszego wykorzystania
-def load_file(path):
-    try:
-        with open(path, 'r', encoding='utf-8') as file:
-            return file.readlines()
-    except Exception as e:
-        print(e)
+class Message:
+    def __init__(self, path, debug):
+        self.path = path
+        self.debug = debug
+        try:
+            with open(self.path, 'r', encoding='utf-8') as file:
+                self.message = file.readlines()
+                if self.debug == True:
+                    print(f'Loaded \"{path}\"')
+        except Exception as e:
+            print(e)
 
 
-#wycięcie fragmentu z nagłówkami i zwrócenie części linijek aż do poczatku body wiadomości
-def find_headers(message):    
-    #usunięcie symboli newline
-    message = [x.replace('\n','') for x in message]
-    
-    #przeszukanie każdego current_rowa w poszukiwaniu zahardcodowanej linijki
-    current_row = 0
-    for row in message:
-        if 'Content-Type: text/plain;' in str(row):
-            return message[0:current_row-1] 
-        else:
-            current_row += 1
+    #wycięcie fragmentu z nagłówkami i zwrócenie części linijek aż do poczatku body wiadomości
+    def find_headers(self):    
+        #usunięcie symboli newline
+        self.message = [x.replace('\n','') for x in self.message]
+        
+        #przeszukanie każdego current_rowa w poszukiwaniu zahardcodowanej linijki
+        current_row = 0
+        for row in self.message:
+            if 'Content-Type: text/plain;' in str(row):
+                if self.debug == True:
+                   print(f'Found header block in \"{self.path}\"') 
+                return self.message[0:current_row-1] 
+            else:
+                current_row += 1
 
 
 #test
-test_file = load_file('pliki_naglowkow//test.txt')
-print(find_headers(test_file))
+test_file = Message('pliki_naglowkow/test.txt', debug=True)
+test_file.find_headers()
